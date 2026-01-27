4978597c2d683b7a999254ee2b931e71cfe8e63f
Martin Damm Laupstad <martin@solcellespesialisten.no>
2026-01-27 12:35:06 -0500

    Added cursor rules and updated agreements index.ts

--- src/components/features/agreements/index.ts
+++ src/components/features/agreements/index.ts
@@ -1,2 +1,5 @@
 export { AgreementCard, AgreementPreviewCard } from "./AgreementCard";
 export { AgreementList } from "./AgreementList";
+export { AgreementWizard } from "./AgreementWizard";
+export { AgreementDetail } from "./AgreementDetail";
+export { AddonSelector } from "./AddonSelector";
